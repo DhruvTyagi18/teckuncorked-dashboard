ab94e37538f94cea973e06199ba33600eaa67d80
DhruvTyagi18 <dhruvtyagi619@gmail.com>
2024-04-13 08:51:28 +0530

    updates

--- tekuncorked-server-main/db.js
+++ tekuncorked-server-main/db.js
@@ -5,7 +5,7 @@ const createDbConnection= async()=> {
 	try{
 		const DB_USERNAME  = process.env.DB_USERNAME; 
 		const DB_PASSWORD  = process.env.DB_PASSWORD;
-		const mongoURI =  `mongodb+srv://@cluster0.r9sixtl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+		const mongoURI =  `mongodb+srv://dhruvtyagi619:dhruv@cluster0.r9sixtl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 		const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 		await mongoose.connect(mongoURI , connectionOptions); 
